3a2e6b0c6c646ba463387372c52375297bf2af1a
AlisonK <AlisonK@ALISON-PC>
2016-12-04 06:20:11 -0200

    Ajuste no Gerenciamento de Despesas 2 e relatórios

--- Mercado/src/controller/visao/estruturais/ControllerViewMenu.java
+++ Mercado/src/controller/visao/estruturais/ControllerViewMenu.java
@@ -2,6 +2,13 @@ package controller.visao.estruturais;
 
 import java.awt.event.ActionEvent;
 import controller.visao.ControllerView;
+import java.io.IOException;
+import java.sql.SQLException;
+import net.sf.jasperreports.engine.JRException;
+import relatorios.Relatorio;
+import relatorios.RelatorioDespesas;
+import relatorios.RelatorioProdutosEmEstoque;
+import relatorios.RelatorioProdutosMaisVendidos;
 import visao.cadastro.ViewCadastroProduto;
 import visao.cadastro.ViewCadastroDespesa;
 import visao.cadastro.ViewCadastroUsuario;
@@ -9,6 +16,8 @@ import visao.consulta.ViewConsultaProduto;
 import visao.consulta.ViewConsultaDespesa;
 import visao.consulta.ViewConsultaUsuario;
 import visao.estruturais.ViewMenu;
+import visao.mensagens.ViewErro;
+import visao.operacoes.ViewOperacaoGerenciarUsuario;
 
 
 /**
@@ -34,6 +43,8 @@ public class ControllerViewMenu extends ControllerView {
             new ViewCadastroUsuario(this.viewMenu).setVisible(true);
         }else if (oEvento.getSource().equals(this.viewMenu.getMenuItemConsultaUsuario())) {
             new ViewConsultaUsuario(this.viewMenu).setVisible(true);
+        }else if (oEvento.getSource().equals(this.viewMenu.getMenuItemOperacoesGerenciarUsuario())) {
+            new ViewOperacaoGerenciarUsuario(this.viewMenu).setVisible(true);
         }else if (oEvento.getSource().equals(this.viewMenu.getMenuItemCadastroProduto())) {
             new ViewCadastroProduto(this.viewMenu).setVisible(true);
         }else if (oEvento.getSource().equals(this.viewMenu.getMenuItemConsultaProduto())) {
@@ -42,6 +53,24 @@ public class ControllerViewMenu extends ControllerView {
             new ViewCadastroDespesa(this.viewMenu).setVisible(true);
         }else if (oEvento.getSource().equals(this.viewMenu.getMenuItemConsultaDespesa())) {
             new ViewConsultaDespesa(this.viewMenu).setVisible(true);
+        }else if (oEvento.getSource().equals(this.viewMenu.getMenuItemRelatoriosProdutosMaisVendidos())) {
+            try {
+                Relatorio oRelatorio = new RelatorioProdutosMaisVendidos();
+            } catch (JRException | IOException | SQLException ex) {
+                new ViewErro(this.viewMenu, "Erro na Geracao do Relatorio!").setVisible(true);
+            } 
+        }else if (oEvento.getSource().equals(this.viewMenu.getMenuItemRelatoriosProdutosEstoque())) {
+            try {
+                Relatorio oRelatorio = new RelatorioProdutosEmEstoque();
+            } catch (JRException | IOException | SQLException ex) {
+                new ViewErro(this.viewMenu, "Erro na Geracao do Relatorio!").setVisible(true);
+            } 
+        }else if (oEvento.getSource().equals(this.viewMenu.getMenuItemRelatoriosDespesa())) {
+            try {
+                Relatorio oRelatorio = new RelatorioDespesas();
+            } catch (JRException | IOException | SQLException ex) {
+                new ViewErro(this.viewMenu, "Erro na Geracao do Relatorio!").setVisible(true);
+            } 
         }
     }
 }
