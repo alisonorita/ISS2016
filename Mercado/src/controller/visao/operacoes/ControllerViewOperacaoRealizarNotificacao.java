25ea201c3714062444807bf7a45107ff2fb94d71
felippefernandes <felippefernandes10@yahoo.com.br>
2016-12-04 23:00:25 -0200

    Adicionando Notificações

---
+++ Mercado/src/controller/visao/operacoes/ControllerViewOperacaoRealizarNotificacao.java
@@ -0,0 +1,66 @@
+package controller.visao.operacoes;
+
+import controller.visao.ControllerViewModal;
+import java.awt.event.ActionEvent;
+import modelo.dao.cadastrais.DaoCliente;
+import modelo.dao.estruturais.DaoAcao;
+import modelo.dao.estruturais.DaoOperacao;
+import modelo.estruturais.Operacao;
+import visao.ajuda.ViewAjudaRealizarNotificacoes;
+import visao.mensagens.ViewErro;
+import visao.mensagens.ViewMensagem;
+import visao.operacoes.ViewOperacaoRealizarNotificacao;
+import visao.pesquisa.ViewPesquisaCliente;
+
+/**
+ * Metodo responsavel por ser o <b>controlador</b> da View para Realizar Notificacoes aos Clientes.
+ * @author Felippe
+ 
+ */
+public class ControllerViewOperacaoRealizarNotificacao extends ControllerViewModal {
+    private final ViewOperacaoRealizarNotificacao viewOperacaoRealizarNotificacao;
+    private final DaoCliente                      daoCliente;
+
+    public ControllerViewOperacaoRealizarNotificacao(ViewOperacaoRealizarNotificacao oView) {
+        super(oView);
+        this.viewOperacaoRealizarNotificacao = oView;
+        this.daoCliente                      = new DaoCliente();
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent oActionEvent) {
+        if (oActionEvent.getSource().equals(this.viewOperacaoRealizarNotificacao.getButtonAddClientes())) {
+            this.viewOperacaoRealizarNotificacao.addClientes();
+        }else if (oActionEvent.getSource().equals(this.viewOperacaoRealizarNotificacao.getButtonAddCliente())) {
+            new ViewPesquisaCliente(this.viewOperacaoRealizarNotificacao).setVisible(true);
+        }else if (oActionEvent.getSource().equals(this.viewOperacaoRealizarNotificacao.getButtonRemoveCliente())) {
+            int iIndex = this.viewOperacaoRealizarNotificacao.getTable().getSelectedRow();
+            if (iIndex >= 0) {
+                this.viewOperacaoRealizarNotificacao.removeCliente(iIndex);
+            }
+        }else if (oActionEvent.getSource().equals(this.viewOperacaoRealizarNotificacao.getButtonRemoveClientes())) {
+            this.viewOperacaoRealizarNotificacao.removeClientes();
+        }else if (oActionEvent.getSource().equals(this.viewOperacaoRealizarNotificacao.getButtonNotificar())) {
+            if (this.checkParameters() == true) {
+                new DaoOperacao().insert(new Operacao("Clientes Notificados = " + this.viewOperacaoRealizarNotificacao.getClientes().size(), new DaoAcao().get(30L), this.viewOperacaoRealizarNotificacao.getUsuario()));
+                new ViewMensagem(this.viewOperacaoRealizarNotificacao, "Notificacao enviada com Sucesso!").setVisible(true);
+                this.viewOperacaoRealizarNotificacao.clear();
+            }
+        }else if (oActionEvent.getSource().equals(this.viewOperacaoRealizarNotificacao.getButtonCancelar())) {
+            this.viewOperacaoRealizarNotificacao.dispose();
+        }else if (oActionEvent.getSource().equals(this.viewOperacaoRealizarNotificacao.getButtonAjuda())) {
+            new ViewAjudaRealizarNotificacoes(this.viewOperacaoRealizarNotificacao).setVisible(true);
+        }
+    }
+    
+    public boolean checkParameters() {
+        if (this.viewOperacaoRealizarNotificacao.getClientes().isEmpty() == true) {
+            new ViewErro(this.viewOperacaoRealizarNotificacao, "Nenhum Cliente Selecionado!").setVisible(true);
+            return false;
+        }else if (this.viewOperacaoRealizarNotificacao.getTextAreaMensagem().getText().trim().equals("") == true) {
+            new ViewErro(this.viewOperacaoRealizarNotificacao, "Digite a Mensagem!").setVisible(true);
+            return false;
+        }
+        return true;
+    }
+}
---
+++ Mercado/src/visao/operacoes/ViewOperacaoRealizarNotificacao.java
@@ -0,0 +1,200 @@
+package visao.operacoes;
+
+import controller.visao.operacoes.ControllerViewOperacaoRealizarNotificacao;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
+import modelo.cadastrais.Cliente;
+import modelo.dao.cadastrais.DaoCliente;
+import modelo.estruturais.Usuario;
+import visao.View;
+import visao.estruturais.ViewMenu;
+
+/**
+ * Classe responsavel por definir a Interface para Realizar Notificacoes aos Clientes do Sistema.
+ * @author  Felippe
+ 
+ */
+public final class ViewOperacaoRealizarNotificacao extends ViewOperacao {
+    private JButton    jButtonAddClientes;
+    private JButton    jButtonAddCliente;
+    private JButton    jButtonRemoveCliente;
+    private JButton    jButtonRemoveClientes;
+    private JTextField jTextFieldTotalClientes;
+    private JTextArea  jTextAreaMensagem;
+    private final DaoCliente daoCliente;
+    private List<Cliente>    clientes;
+    private final ViewMenu   viewMenu;
+
+    public ViewOperacaoRealizarNotificacao(View oViewParent) {
+        super(oViewParent);
+        this.viewMenu   = (ViewMenu) oViewParent;
+        this.controller = new ControllerViewOperacaoRealizarNotificacao(this);
+        this.daoCliente = new DaoCliente();
+        this.initComponents();
+    }
+
+    @Override
+    public void initComponents() {
+        this.setTitle("Mercado - Realizar Notificacoes");
+        this.setLocation(320, 80);
+        this.setSize(500, 590);
+        this.addHeader();
+        this.addComponents();
+        this.addButtons();
+        this.clear();
+    }
+    
+    @Override
+    public void addHeader() {
+        super.addHeader("realizar_notificacao.jpg");
+    }
+
+    @Override
+    public void addComponents() {
+        
+        this.addTable();
+        String[] sDados = {"Id", "Documento", "Nome"};
+        this.setColumns(sDados);
+        this.jScrollPane.setPreferredSize(new Dimension(350, 150));
+        
+        int[]    iColumns = {75, 175, 175};
+        this.setColumnSize(iColumns);
+        
+        this.jButtonAddClientes    = this.createButton("", "add_clientes.jpg");
+        this.add(this.jButtonAddClientes);
+        this.jButtonAddCliente     = this.createButton("Adicionar", "add.jpg");
+        this.add(this.jButtonAddCliente);
+        this.jButtonRemoveCliente  = this.createButton(" Remover ", "exit.jpg");
+        this.add(this.jButtonRemoveCliente);
+        this.jButtonRemoveClientes = this.createButton("", "remove_clientes.jpg");
+        this.add(this.jButtonRemoveClientes);
+        
+        this.addLinhas(1);
+        
+        this.jTextFieldTotalClientes  = new JTextField(10);
+        this.jTextFieldTotalClientes.setEditable(false);
+        this.add(new JLabel("Total Clientes: "));
+        this.add(this.jTextFieldTotalClientes);
+        
+        this.addLinhas(1);
+        
+        this.jTextAreaMensagem        = new JTextArea(5, 26);
+        this.jTextAreaMensagem.setBorder(new LineBorder(Color.GRAY));
+        this.add(new JLabel("Mensagem: "));
+        this.add(this.jTextAreaMensagem);
+        
+        this.addLinhas(1);
+    }
+    
+    @Override
+    public void addButtons() {
+        this.jButtonAction1 = this.createButton("Notificar", "email.jpg");
+        this.jButtonAction2 = this.createButton(" Cancelar", "back.jpg");
+        this.jButtonAction3 = this.createButton(""         , "help.jpg");
+        
+        this.add(this.jButtonAction1);
+        this.add(this.jButtonAction2);
+        this.add(this.jButtonAction3);
+    }
+
+    @Override
+    public void clear() {
+        this.addClientes();
+        this.jTextAreaMensagem.setText("");
+    }
+
+    /**
+     * Metodo responsavel por adicionar todos os Clientes a Tabela.
+     */
+    public void addClientes() {
+        this.clientes = this.daoCliente.list();
+        this.addRows(this.daoCliente.getClientes(this.clientes));
+        this.jTextFieldTotalClientes.setText(Integer.toString(this.clientes.size()));
+    }
+    
+    /**
+     * Metodo responsavel por adicionar um Cliente a Tabela.
+     * @param oCliente Cliente.
+     */
+    public void addCliente(Cliente oCliente) {
+        if (this.clientes.contains(oCliente) == false) {
+            this.clientes.add(oCliente);
+        }
+        this.addRows(this.daoCliente.getClientes(this.clientes));
+        this.jTextFieldTotalClientes.setText(Integer.toString(this.clientes.size()));
+    }
+    
+    /**
+     * Metodo responsavel por remover um Cliente da Tabela.
+     * @param iIndex Indice da Tabela.
+     */
+    public void removeCliente(int iIndex) {
+        this.clientes.remove(iIndex);
+        this.addRows(this.daoCliente.getClientes(this.clientes));
+        this.jTextFieldTotalClientes.setText(Integer.toString(this.clientes.size()));
+    }
+    
+    /**
+     * Metodo responsavel por remover todos os Clientes da Tabela.
+     */
+    public void removeClientes() {
+        this.clientes = new ArrayList<>();
+        this.addRows(this.daoCliente.getClientes(this.clientes));
+        this.jTextFieldTotalClientes.setText(Integer.toString(this.clientes.size()));
+    }
+    
+    public void setMessage(String sMessage) {
+        this.jTextAreaMensagem.setText(sMessage);
+    }
+
+    public void setClientes(List<Cliente> oClientes) {
+        this.clientes = oClientes;
+    }
+    
+    public List<Cliente> getClientes() {
+        return this.clientes;
+    }
+    
+    public JButton getButtonAddClientes() {
+        return this.jButtonAddClientes;
+    }
+
+    public JButton getButtonAddCliente() {
+        return this.jButtonAddCliente;
+    }
+
+    public JButton getButtonRemoveCliente() {
+        return this.jButtonRemoveCliente;
+    }
+
+    public JButton getButtonRemoveClientes() {
+        return this.jButtonRemoveClientes;
+    }
+
+    public JTextArea getTextAreaMensagem() {
+        return this.jTextAreaMensagem;
+    }
+
+    public JButton getButtonNotificar() {
+        return this.jButtonAction1;
+    }
+
+    public JButton getButtonCancelar() {
+        return this.jButtonAction2;
+    }
+    
+    public JButton getButtonAjuda() {
+        return this.jButtonAction3;
+    }
+    
+    public Usuario getUsuario() {
+        return this.viewMenu.getUsuario();
+    }
+}
